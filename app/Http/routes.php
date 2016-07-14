8259644ab46f2a76f116add0fa359c684818c49d
luoxiaojing <1318281150@qq.com>
2016-07-14 11:04:24 +0800

    laravel开发联系

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hi', function () {
+    return view('hi')->with('title','hello world');
+});
+
+
+
---
+++ resources/views/hi.blade.php
@@ -0,0 +1,4 @@
+@include('header')
+<h1>{{$title}}</h1>
+<sapn>我的第一个文件</sapn>
+@include('footer')
